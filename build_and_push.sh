e6649c12435accb10a05948dff7e5f87c86c8b34
Andrew Petrov <andrew.petrov@sumsub.com>
2024-07-01 22:54:21 +0200

    Improve build and deploy script
    Add tags to the script

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 TAG=${1:latest}
 FULL_TAG=ghcr.io/finance-control/bookkeeper:"${TAG}"
+VERSION="v${TAG}"
+
+git tag "${VERSION}" -m "${2}" && git push --tags
+
+if [[ $? -ne 0 ]]; then
+    echo "smth went wrong"
+    exit 1
+fi
 
 ./gradlew bootJar
 docker build . -t "$FULL_TAG" --platform linux/amd64 -f Dockerfile-light
